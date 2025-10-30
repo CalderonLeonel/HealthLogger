fb8a60aec4dbd5dc7f527ba09c72b1b773a1018e
CalderonLeonel <CRL1009575@est.univalle.edu>
2025-10-29 20:50:47 -0400

    Limpieza de librerias

--- HealthLogger/Converters/EstadoToToolTipConverter.cs
+++ HealthLogger/Converters/EstadoToToolTipConverter.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Windows.Data;
-using System.Diagnostics;
 namespace HealthLogger.Converters
 {
     public class EstadoToToolTipConverter : IValueConverter
